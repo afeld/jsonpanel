dcfc0cf44876b91b8a987e01a7547ce331861cce
Aidan Feldman <afeld@github.com>
2014-07-01 01:20:20 -0400

    more tests

--- tests.js
+++ tests.js
@@ -1,8 +1,27 @@
-(function(){
+$(function(){
   var $fixture = $('#qunit-fixture');
 
   QUnit.test("returns a Panel instance", function(assert){
-    var result = $fixture.jsonpanel({});
-    assert.equal(typeof result.render, 'function');
+    var panel = $fixture.jsonpanel({});
+    assert.equal(typeof panel.render, 'function');
   });
-})();
+
+  QUnit.test("starts off collapsed", function(assert){
+    var panel = $fixture.jsonpanel({
+      obj: {
+        foo: 'bar'
+      }
+    });
+    assert.equal($fixture.find('li.selected').length, 0);
+  });
+
+  QUnit.test("expands when clicked", function(assert){
+    var panel = $fixture.jsonpanel({
+      obj: {
+        foo: 'bar'
+      }
+    });
+    $fixture.find('.expandable').click();
+    assert.equal($fixture.find('li.selected').length, 1);
+  });
+});
