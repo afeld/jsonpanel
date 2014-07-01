347dc5aa65ebd6f478cdfe75d86b2cfa585d089e
Aidan Feldman <afeld@github.com>
2014-07-01 03:21:05 -0400

    add tests for arrays vs objects

--- tests.js
+++ tests.js
@@ -6,7 +6,7 @@ $(function(){
     assert.strictEqual(typeof panel.render, 'function');
   });
 
-  QUnit.test("starts off collapsed", function(assert){
+  QUnit.test("object starts off collapsed", function(assert){
     var panel = $fixture.jsonpanel({
       obj: {
         foo: 'bar'
@@ -15,11 +15,26 @@ $(function(){
     assert.strictEqual($fixture.find('li.expanded').length, 0);
   });
 
-  QUnit.test("adds a .expanded class when clicked", function(assert){
+  QUnit.test("array starts off collapsed", function(assert){
+    var panel = $fixture.jsonpanel({
+      ary: ['foo']
+    });
+    assert.strictEqual($fixture.find('li.expanded').length, 0);
+  });
+
+  QUnit.test("adds a .expanded class when object is clicked", function(assert){
     var panel = $fixture.jsonpanel({
       obj: {
         foo: 'bar'
       }
+    });
+    $fixture.find('.expandable').click();
+    assert.strictEqual($fixture.find('li.expanded').length, 1);
+  });
+
+  QUnit.test("adds a .expanded class when array is clicked", function(assert){
+    var panel = $fixture.jsonpanel({
+      ary: ['foo']
     });
     $fixture.find('.expandable').click();
     assert.strictEqual($fixture.find('li.expanded').length, 1);
